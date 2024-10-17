c47974a74d13d5669953ed023022db90c9b60d92
Lyara-Ribeiro <lyaragithub@gmail.com>
2024-10-17 12:29:02 -0300

    Desenvolvimento da API de clientes

--- src/main/java/br/com/cotiinformatica/entities/Cliente.java
+++ src/main/java/br/com/cotiinformatica/entities/Cliente.java
@@ -4,56 +4,69 @@ import java.util.Date;
 import java.util.UUID;
 
 public class Cliente {
-	
+
 	private UUID id;
 	private String nome;
 	private String email;
 	private String telefone;
-	private Date datacadastro;
-	
-	public Cliente(){
-		
-		
+	private Date dataCadastro;
+
+	public Cliente() {
+		// TODO Auto-generated constructor stub
 	}
-	public Cliente(UUID id, String nome, String email, String telefone, Date datacadastro){
-		
-		
+
+	public Cliente(UUID id, String nome, String email, String telefone, Date dataCadastro) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.telefone = telefone;
+		this.dataCadastro = dataCadastro;
 	}
+
 	public UUID getId() {
 		return id;
 	}
+
 	public void setId(UUID id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getTelefone() {
 		return telefone;
 	}
+
 	public void setTelefone(String telefone) {
 		this.telefone = telefone;
 	}
-	public Date getDatacadastro() {
-		return datacadastro;
+
+	public Date getDataCadastro() {
+		return dataCadastro;
 	}
-	public void setDatacadastro(Date datacadastro) {
-		this.datacadastro = datacadastro;
+
+	public void setDataCadastro(Date dataCadastro) {
+		this.dataCadastro = dataCadastro;
 	}
+
 	@Override
 	public String toString() {
 		return "Cliente [id=" + id + ", nome=" + nome + ", email=" + email + ", telefone=" + telefone
-				+ ", datacadastro=" + datacadastro + "]";
+				+ ", dataCadastro=" + dataCadastro + "]";
 	}
-
-
 }
--- src/main/java/br/com/cotiinformatica/repositories/ClienteRepository.java
+++ src/main/java/br/com/cotiinformatica/repositories/ClienteRepository.java
@@ -1,7 +1,9 @@
 package br.com.cotiinformatica.repositories;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.UUID;
-
 import br.com.cotiinformatica.entities.Cliente;
 import br.com.cotiinformatica.factories.ConnectionFactory;
 
@@ -55,29 +57,97 @@ public class ClienteRepository {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/*
 	 * Método para excluir um cliente no banco de dados
 	 */
 	public void delete(UUID id) {
-		
+
 		try {
-			
+
 			var connectionFactory = new ConnectionFactory();
 			var connection = connectionFactory.getConnection();
-			
+
 			var statement = connection.prepareStatement("DELETE FROM cliente WHERE id=?");
 			statement.setString(1, id.toString());
 			statement.execute();
-			
+
 			connection.close();
-		}
-		catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
+	/*
+	 * Método para consultar todos os clientes no banco de dados
+	 */
+	public List<Cliente> findAll() {
 
+		try {
 
+			var connectionFactory = new ConnectionFactory();
+			var connection = connectionFactory.getConnection();
+
+			var statement = connection.prepareStatement("SELECT * FROM cliente ORDER BY nome");
+			var resultSet = statement.executeQuery(); // executando a consulta no banco de dados
+
+			var lista = new ArrayList<Cliente>(); // declarando uma lista de clientes
+
+			while (resultSet.next()) { // percorrendo a consulta
+
+				var cliente = new Cliente();
+
+				cliente.setId(UUID.fromString(resultSet.getString("id")));
+				cliente.setNome(resultSet.getString("nome"));
+				cliente.setEmail(resultSet.getString("email"));
+				cliente.setTelefone(resultSet.getString("telefone"));
+				cliente.setDataCadastro(new Date(resultSet.getTimestamp("datacadastro").getTime()));
+
+				lista.add(cliente); // adicionando cada cliente na lista
+			}
+
+			connection.close(); // fechando conexão com o banco de dados
+
+			return lista; // retornando a lista de clientes obtidos
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+
+	/*
+	 * Método para consultar 1 cliente no banco de dados através do ID
+	 */
+	public Cliente findById(UUID id) {
+
+		try {
+
+			var connectionFactory = new ConnectionFactory();
+			var connection = connectionFactory.getConnection();
+
+			var statement = connection.prepareStatement("SELECT * FROM cliente WHERE id=?");
+			statement.setString(1, id.toString());
+			var resultSet = statement.executeQuery(); // executando a consulta no banco de dados
+
+			Cliente cliente = null; // vazio
+
+			if (resultSet.next()) { // verificando se algum cliente foi encontrado
+				cliente = new Cliente(); // instanciando o objeto Cliente
+
+				cliente.setId(UUID.fromString(resultSet.getString("id")));
+				cliente.setNome(resultSet.getString("nome"));
+				cliente.setEmail(resultSet.getString("email"));
+				cliente.setTelefone(resultSet.getString("telefone"));
+				cliente.setDataCadastro(new Date(resultSet.getTimestamp("datacadastro").getTime()));
+			}
+
+			connection.close(); // fechando a conexão com o banco de dados
+
+			return cliente; // retornando o cliente
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 }
